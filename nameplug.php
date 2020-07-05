63d1456302c21a9a0c1fe92721c35bb2f49fcbe3
suyash0103 <suyashghuge@gmail.com>
2020-07-05 13:09:18 +0530

    name plug modification

--- nameplug.php
+++ nameplug.php
@@ -7,33 +7,26 @@
     Version: 1.0
     Author URI: http://localhost/wp-vs-352/wordpress/wp-admin/
     */
-    add_action( 'the_content', 'my_thank_you_text' );
+    add_action( 'the_content', 'display_name' );
+    
+    add_action('admin_menu', 'addName');
+	function addName() {
+	  add_menu_page('Name', 'Name', 'manage_options' ,__FILE__, 'display_name', 'dashicons-wordpress');
+	}
 
-	function my_thank_you_text ( $content ) {
+	function display_name ( $content ) {
 		global $current_user; wp_get_current_user(); 
-
-		echo 'Following are your details:';
-		echo "<br>";
 
 		if ( is_user_logged_in() ) 
 		{ 
-			echo 'Username: ' . $current_user->user_login . "\n"; 
+			echo 'Hello ' . $current_user->user_login . "\n"; 
 			echo "<br>";
-			echo 'User display name: ' . $current_user->display_name . "\n"; 
+			echo 'Hello ' . $current_user->display_name . "\n"; 
 		} 
 		else 
 		{ 
 			wp_loginout(); 
-		} 
-	    // return $content .= '<p>Hello World!</p>';
+		}
 	}
-
-	// wp_get_current_user();
-	// echo 'Username: ' . $current_user->user_login . "\n";
-	// echo 'User display name: ' . $current_user->display_name . "\n";
-	// <?php 
-
-		
-
 ?>
 
